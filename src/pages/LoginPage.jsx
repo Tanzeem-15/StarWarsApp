08ed40b4b2069b7204fe0242a1fad3d6fb1deb2d
akshat <akshat.g1994@gmail.com>
2025-08-10 12:22:45 +0530

    Added brackets

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import LoginForm from '../components/LoginForm';
 
 export default function LoginPage({ navigation }) {
-    return <LoginForm navigation={navigation} />;
+    return (<LoginForm navigation={navigation} />);
 }
